3ff1a71ee58bd1b3c91d4a7f9d0a77adae49e43b
Emily R <emilyroulund@gmail.com>
2019-04-20 02:30:55 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,19 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves (dwarf_names)
+  puts .each_with_index (dwarf_names) |name, index + 1|
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  # capitalize each element and add an exclamation point at the end
+  # map or collect 
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(planeteer_calls)
+  # tell us if any of the calls are longer than four characters
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array_of_strings)
+  # find and return the first string that is a type of cheese.
   cheese_types = ["cheddar", "gouda", "camembert"]
+  #If, sadly, a list of ingredients does not include cheese, return nil 
+  # include method for him 
 end
